d00d4dae8f1f03d7f7fd5f96aa30534cbcc6c714
catalina <catab.o205@gmail.com>
2025-12-26 05:32:47 +0100

    revisar mañana con menos sueño (meter reproductor de musica)

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// MENU RESPONSIVE
+
 $(document).ready(function () {
     $("#menu-toggle").on("click", function () {
         $("#nav-links-responsive").toggleClass("active");
@@ -7,3 +9,20 @@ $(document).ready(function () {
         $("#nav-links-responsive").removeClass("active");
     });
 });
+
+
+// MENU
+
+const menu = document.getElementById("menu");
+const trigger = document.querySelector(".cantantes");
+
+window.addEventListener("scroll", () => {
+    const triggerTop = trigger.offsetTop;
+
+    if (window.scrollY >= triggerTop - 100) {
+        menu.classList.add("top");
+    } else {
+        menu.classList.remove("top");
+    }
+});
+
